c5d2bc2a35724d3604dcefcabee3ded951692d75
Lars Bonnefoy <lars.bonnefoy@hotmail.com>
2023-05-19 18:21:29 +0200

    ex00 done i guess

---
+++ CPP05/ex00/Sources/Bureaucrat.cpp
@@ -0,0 +1,70 @@
+#include "../Includes/Bureaucrat.hpp"
+#include<string>
+#include<exception>
+#include<iostream>
+
+Bureaucrat::Bureaucrat(void)
+    : _name("Default"), _grade(150){
+}
+//this->_garde will be an undefined value, set it to smtgh?
+//catch it in the constructor or in the calling function?
+Bureaucrat::Bureaucrat(std::string name, int grade)
+    : _name(name) {
+    std::cout << "Full Constructor of " << name << " Called" << std::endl;
+    if (grade > 150) {
+        throw GradeTooLowException();
+    }
+    else if (grade < 1) {
+        throw GradeTooHighException();
+    }
+    this->_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+    : _name(other._name), _grade(other._grade) {
+}
+
+Bureaucrat::~Bureaucrat(void) {
+    std::cout << "Default destructor of " << this->getName() << " Called" << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other) {
+    this->_grade = other._grade;
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &out,const Bureaucrat &b) {
+    out << b.getName() << ", bureaucrat grade " << b.getGrade() << ".";
+    return (out);
+}
+
+//################MEMBER FUNC#######################// 
+std::string Bureaucrat::getName(void) const {
+    return this->_name;
+}
+
+int Bureaucrat::getGrade(void) const {
+    return this->_grade;
+}
+
+void Bureaucrat::incrGrade(void) {
+    if (--this->_grade < 1) {
+        throw GradeTooHighException();
+    }
+}
+
+void Bureaucrat::decrGrade(void) {
+    if (++this->_grade > 150) {
+        throw GradeTooLowException();
+    }
+}
+
+//################EXCEPTION#######################// 
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+    return("[Bureaucrat::GradeTooLowException] : Cannot assign grade lower than 150.");
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+    return("[Bureaucrat::GradeTooHighException] : Cannot assign grade higher than 1.");
+}
---
+++ CPP05/ex00/Sources/main.cpp
@@ -0,0 +1,54 @@
+#include "../Includes/Bureaucrat.hpp"
+#include <exception>
+#include <iostream>
+
+//dans le cas d'un catch, supprimer la memoire, la rendre inaccessible??
+//comment gerer le cas ou le code s'est mal comporte
+//=> tout mettre dans le meme bloc de try, en effet s'il y a une erreur
+//on va directement sortir du block => on va pas acceder aux lignes de codes en dessous
+int main(void){
+    std::cout << "################COPY and ASSIGNMENT constructors#############" << std::endl;
+    Bureaucrat A("A", 100);
+    std::cout << A << std::endl;
+
+    Bureaucrat B(A);
+    std::cout << B << std::endl;
+
+    Bureaucrat C;
+    C = A;
+    std::cout << C << std::endl;
+    std::cout << "-------------------------------------------------------------" << std::endl << std::endl;
+
+    try {
+        Bureaucrat chef("Michelle", 2);
+        Bureaucrat notAChef("Remy", 149);
+        Bureaucrat smallBoy("smallBoy", 1000000); //quand qqch est dans un block try on peut pas y acceder de l'exterieur
+        Bureaucrat bigBoy("smallBoy", 0); //this line is never read as the above line throws an exception
+        std::cout << smallBoy << std::endl; 
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    //std::cout << test << std::endl; //=>impossible d'imprimer ca car pas dans le bloc try catch 
+    try {
+        Bureaucrat chef("Michelle", 2);
+        chef.incrGrade();
+        std::cout << chef << std::endl;
+        chef.incrGrade();
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    try {
+        Bureaucrat notAChef("Remy", 149);
+        notAChef.decrGrade();
+        std::cout << notAChef << std::endl;
+        notAChef.decrGrade();
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << std::endl << "################COPY and ASSIGNMENT object destructors###############" << std::endl;
+    return (0);
+}
